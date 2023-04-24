961ad5823029b95c29bac02069b9c5c18c83a615
mdcrowley <matthew.crowley@tri-c.edu>
2023-04-23 20:20:03 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,12 @@
+
+function salesByPerson() {
+   alert("Display data in this alert box.");
+}
+
+function bestAndWorst() {
+   alert("Display data in this alert box.");
+}
+
+function commissionsByMonth() {
+   alert("Display data in this alert box.");
+}
